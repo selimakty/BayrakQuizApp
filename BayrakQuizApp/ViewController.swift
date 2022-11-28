7e7d013a744aad4a0404c5e226bb26400592fb0d
Selim Aktay <sellimakty99@gmail.com>
2022-11-28 16:48:46 +0300

    Initial Commit

---
+++ BayrakQuizApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BayrakQuizApp
+//
+//  Created by Selim on 28.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
